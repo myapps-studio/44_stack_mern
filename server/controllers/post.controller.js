0a1e35da4c3f4dc6e88e619a211eb5eefad5f19f
Andrzej Mieczkowski <praca.mieczkowski@gmail.com>
2019-09-22 15:37:20 +0200

    server changes

---
+++ server/controllers/post.controller.js
@@ -0,0 +1,13 @@
+const Post = require('../models/post.model');
+
+// get all posts
+
+exports.getPosts = async (req, res) => {
+
+    try {
+      res.status(200).json(await Post.find());
+    } catch(err) {
+      res.status(500).json(err);
+    }
+
+};
